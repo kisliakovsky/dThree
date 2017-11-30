342298248575a5bc55f2df7a9e2683a4f236cef6
kisliakovsky <kisliakovskiii@yandex.ru>
2017-11-30 14:25:04 +0300

    WebPack config

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+module.exports = [
+    {
+        "entry": {
+            "app": ["./lib/main.js"]
+        },
+
+        "output": { // Transpiled and bundled output gets put in `build/bundle.js`.
+            "path": path.resolve(__dirname, "build"),
+            "publicPath": "/assets/",
+            "filename": "bundle.js"
+        },
+
+        "devtool": "inline-source-map",
+
+        "module": {
+            "rules": [
+                {
+                    "test": /\.js?$/,
+                    "exclude": [/(node_modules|bower_components)/],
+                    "loader": "babel-loader"
+                },
+                {
+                    "test": /\.json$/,
+                    "loader": "json-loader"
+                },
+                {
+                    "test": /\.css$/,
+                    "loader": "style-loader!css-loader"
+                }
+            ]
+        }
+    }
+];
