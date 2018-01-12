46c5f7b0fd7b8429dd07c03f7066e95d736ee1ce
Ilia Kisliakovskii <kisliakovskiii@niuitmo.ru>
2018-01-12 21:52:27 +0300

    webpack update, sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,43 @@
-const path = require("path");
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = [
-    {
-        entry: {
-            app: ["./lib/main.js"]
+  {
+    entry: {
+      app: ['./lib/main.js'],
+    },
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      publicPath: '/assets/',
+      filename: 'bundle.js',
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+      new ExtractTextPlugin('index.css'),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js?$/,
+          exclude: [/(node_modules|bower_components)/],
+          loader: 'babel-loader',
         },
-        output: {
-            path: path.resolve(__dirname, "build"),
-            publicPath: "/assets/",
-            filename: "bundle.js"
+        {
+          test: /\.json$/,
+          loader: 'json-loader',
         },
-        devtool: "inline-source-map",
-        module: {
-            rules: [
-                {
-                    test: /\.js?$/,
-                    exclude: [/(node_modules|bower_components)/],
-                    loader: "babel-loader"
-                },
-                {
-                    test: /\.json$/,
-                    loader: "json-loader"
-                },
-                {
-                    test: /\.css$/,
-                    loader: "style-loader!css-loader"
-                },
-                {
-                    test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-                    use: 'file-loader'
-                }
-            ]
-        }
-    }
+        {
+          test: /\.scss$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader', 'sass-loader'],
+          }),
+        },
+        {
+          test: /\.ttf$/,
+          loader: 'file?name=font/[name].[ext]',
+        },
+      ],
+    },
+  },
 ];
